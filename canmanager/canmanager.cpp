9f044fee1be1db63857fb14187be718f83ca617b
Aditya YV <adityayv802@gmail.com>
2024-10-08 19:01:28 -0500

    Re-did comments, emptied queue in Destructor and added a condition in ISR

--- canmanager/canmanager.cpp
+++ canmanager/canmanager.cpp
@@ -2,12 +2,14 @@
 
 
 void CANManager::readISR() {
-    // TODO: read CAN message and enqueue data
+    // Read CAN message and enqueue data
     CAN_message_t CAN_message;
-    this->canBus.read(CAN_message); 
+    bool check = this->canBus.read(CAN_message);  //returns true if message is read successfully
     
-    CAN_data toQueue= {CAN_message.id, *CAN_message.buf, CAN_message.len}; //Dereferencing CAN_message.buf to convert it to uint8_t from uint8_t*
-    this->messageQueue.push(toQueue);
+    if (check){
+        CAN_data toQueue= {CAN_message.id, *CAN_message.buf, CAN_message.len}; //Dereferencing CAN_message.buf to convert it to uint8_t from uint8_t*
+        this->messageQueue.push(toQueue);
+    }
 } 
 
 CANManager::CANManager(CAN_TypeDef* canPort, CAN_PINS pins, uint8_t rx, int frequency) : canBus(canPort, pins), messageQueue() {
@@ -23,35 +25,47 @@ CANManager::CANManager(CAN_TypeDef* canPort, CAN_PINS pins, uint8_t rx, int freq
 CANManager::~CANManager() {
     //Clear interrupt handlers
     this->canBus.disableMBInterrupts();
+
+    //Empties queue
+    while (!this->messageQueue.empty()) {
+        this->messageQueue.pop();
+    }
 }
 
 int CANManager::sendMessage(int messageID, void* data, int length, int timeout) {
-    // TODO: use a Ticker to handle the timeout
+    // Storing the time when the function is called
     unsigned long start = millis();
-    // TODO: make sure to runQueue in between attempts to send.
-    bool retValue = false;
-    CAN_message_t CAN_message;
+    
+    bool retValue = false;                  //return value of write function. False by default
+
+    // Create a CAN message that is going to be written
+    CAN_message_t CAN_message;                
     CAN_message.id = messageID;
     CAN_message.len = length;
+
+    // Copy the data to the buffer
     for (int i = 0; i < length; i++) {
         CAN_message.buf[i] = ((uint8_t*)data)[i];
     }
+
+    // Keep trying to write the message until it is successful or timeout
     while (!(retValue = this->canBus.write(CAN_message)) && millis() - start < timeout){
         this->runQueue(1);
     }
-    return retValue;
+
+    return retValue;  //returns true if message is written successfully
 }
 
 void CANManager::runQueue(int duration) {
-    // TODO: use Ticker to handle the duration
+    // Storing the time when the function is called
     unsigned long start = millis();
 
-    // TODO: dequeue a CAN message, and call readHandler to deal with the CAN message accordingly
+    // dequeues messageQueue until the duration is reached
     while (millis() - start < duration){
         if (!this->messageQueue.empty()) {
             CAN_data msg = this->messageQueue.front();
             this->messageQueue.pop();
-            this->readHandler(msg);                 //virtual void readHandler needed in header maybe?
+            this->readHandler(msg);    //calls readHandler function to process the message              
         }
     }   
 }
