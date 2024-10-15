adfb121686bc204378ac39c4631ffd92736da0d9
bquan0 <powerbowen@gmail.com>
2024-10-14 22:53:14 -0500

    remove IRQ and custom struct for CAN data

--- canmanager/canmanager.cpp
+++ canmanager/canmanager.cpp
@@ -1,42 +1,12 @@
 #include "canmanager.h"
 
-
-void CANManager::readISR() {
-    // Read CAN message and enqueue data
-    CAN_message_t CAN_message;
-
-    bool check = this->canBus.read(CAN_message);  //returns true if message is read successfully
-
-    if (check){
-        CAN_data toQueue= {CAN_message.id, CAN_message.buf, CAN_message.len};
-        this->messageQueue.push(toQueue);
-    }
-} 
-
-CANManager::CANManager(CAN_TypeDef* canPort, CAN_PINS pins, uint8_t rx, int frequency) : canBus(canPort, pins), messageQueue() {
-    // Attach RX pin and handler function to interrupt
-    attachInterrupt(digitalPinToInterrupt(rx), [this]() { this->readISR(); }, RISING || FALLING);   //attaches an interrupt to handler (readISR) using lambda function and calls it on rising or falling edge
-    this->canBus.enableMBInterrupts();                                                              //enables mailbox interrupts
-
+CANManager::CANManager(CAN_TypeDef* canPort, CAN_PINS pins, int frequency) : canBus(canPort, pins) {                                                            
     // Begin canBus and set its frequency
     this->canBus.begin();
     this->canBus.setBaudRate(frequency);
 }
 
-CANManager::~CANManager() {
-    //Clear interrupt handlers
-    this->canBus.disableMBInterrupts();
-
-    //Empties queue
-    while (!this->messageQueue.empty()) {
-        this->messageQueue.pop();
-    }
-}
-
-int CANManager::sendMessage(int messageID, void* data, int length, int timeout) {
-    // Storing the time when the function is called
-    unsigned long start = millis();
-    
+bool CANManager::sendMessage(int messageID, void* data, int length, int timeout) {
     bool retValue = false;                  //return value of write function. False by default
 
     // Create a CAN message that is going to be written
@@ -49,6 +19,9 @@ int CANManager::sendMessage(int messageID, void* data, int length, int timeout) 
         CAN_message.buf[i] = ((uint8_t*)data)[i];
     }
 
+    // Storing the time right before we try sending
+    unsigned long start = millis();
+
     // Keep trying to write the message until it is successful or timeout
     while (!(retValue = this->canBus.write(CAN_message)) && millis() - start < timeout){
         this->runQueue(1);
@@ -60,13 +33,12 @@ int CANManager::sendMessage(int messageID, void* data, int length, int timeout) 
 void CANManager::runQueue(int duration) {
     // Storing the time when the function is called
     unsigned long start = millis();
+    CAN_message_t msg; 
 
     // dequeues messageQueue until the duration is reached
     while (millis() - start < duration){
-        if (!this->messageQueue.empty()) {
-            CAN_data msg = this->messageQueue.front();
-            this->messageQueue.pop();
-            this->readHandler(msg);    //calls readHandler function to process the message              
+        if (this->canBus.read(msg)) {
+            this->readHandler(msg); //calls readHandler function to process the message              
         }
     }   
 }
--- canmanager/canmanager.h
+++ canmanager/canmanager.h
@@ -7,19 +7,9 @@
 
 #define DEFAULT_CAN_FREQ 250000     // Match Elcon charger
 
-typedef struct {
-    uint32_t id;
-    uint8_t* buf;  
-    uint8_t len;
-} CAN_data;
-
 class CANManager {
     private:
-        std::queue<CAN_data> messageQueue; // CAN messages we read
         STM32_CAN canBus;                  // object to interface with CAN
-        
-        // Interrupt Service Routine/Handler to read CAN message and put the data in a queue
-        void readISR(); 
 
     public:
         /* Constructor initializing bus and all manager functions
@@ -27,17 +17,13 @@ class CANManager {
          * canPort: choose from CAN1, CAN2, CAN3 (NOTE: see STM32_CAN library for more details)
          * pins: choose from DEF, ALT1, ALT2
          * frequency: Baud rate of can bus
-         * rx: digital pin number to attach interrupt to
          */
-        CANManager(CAN_TypeDef* canPort, CAN_PINS pins, uint8_t rx, int frequency = DEFAULT_CAN_FREQ);
-
-        // Destructor stopping manager and freeing resources
-        ~CANManager();
+        CANManager(CAN_TypeDef* canPort, CAN_PINS pins, int frequency = DEFAULT_CAN_FREQ);
 
         /* Reads input message and does any logic handling needed
          * Intended to be implemented by class extension per board
          */
-        virtual void readHandler(CAN_data msg) = 0;
+        virtual void readHandler(CAN_message_t msg) = 0;
 
         /* Send a message over CAN
          *
@@ -46,7 +32,7 @@ class CANManager {
          * length: Size of data in bytes
          * timeout: in milliseconds
          */ 
-        int sendMessage(int messageID, void* data, int length, int timeout = 10);
+        bool sendMessage(int messageID, void* data, int length, int timeout = 10);
 
         /* Processes CAN (read) messages stored in messageQueue for a set duration. 
          * THIS IS THE FUNCTION TO CALL FOR PROCESSING CAN READ MESSAGES
